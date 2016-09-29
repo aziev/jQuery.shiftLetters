eae80e6a04002c321fad60aae18216f6ece86563
mik <mikail.aziev@gmail.com>
2016-09-29 17:34:31 +0300

    upload files

---
+++ jquery.amrShiftLetters.js
@@ -0,0 +1,65 @@
+;(function ($) {
+
+    $.fn.amrShiftLetters = function (options) {
+
+        var settings = $.extend({
+            minOffset: 2,
+            maxOffset: 5,
+            easing: 'swing',
+        }, options);
+
+        return this.each(function() {
+
+            var letters = $(this).text().trim().split('');
+            var $container = $('<div/>').css('height', '1em');
+            var $this = $(this);
+
+            $(letters).each(function(index){
+
+                if (this[0] == ' ')
+                {
+                    $container.append(' ');
+                    return;
+                }
+
+                var offset = Math.round(Math.random() * (settings.maxOffset - settings.minOffset) + settings.minOffset);
+                var maskHeight = (offset - 1) * 2 + 1;
+                var sign = index % 2 ? 1 : -1;
+
+                $mask = $('<p/>').css({
+                    display: 'inline-block',
+                    verticalAlign: 'top',
+                    margin: '0',
+                    marginTop: Math.floor(maskHeight / 2) * -1 + 'em',
+                    height: maskHeight + 'em',
+                    overflowY: 'hidden',
+                });
+
+                $('<i/>', {
+                    text: this[0],
+                }).css({
+                    fontStyle: 'normal',
+                    position: 'relative',
+                    top: (index % 2 ? -1 : maskHeight+1) + 'em',
+                }).appendTo($mask).delay(100).animate({
+                    top: offset - 1 + 'em',
+                }, {
+                    duration: 800,
+                    easing: settings.easing,//'easeOutQuint',
+                });
+
+                $mask.appendTo($container);
+
+                setTimeout(function(){
+                    $this.text(letters.join(''));
+                }, 900);
+
+            });
+
+            $(this).html($container);
+
+        });
+
+    };
+
+})(jQuery);
